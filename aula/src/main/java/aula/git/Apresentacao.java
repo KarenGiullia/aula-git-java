8b874dfaea105836d54fb548061accf072c6773c
KarenGiullia <karen.amorim1010@gmail.com>
2024-03-26 10:19:49 -0300

    Melhor descrição

--- aula/src/main/java/aula/git/Apresentacao.java
+++ aula/src/main/java/aula/git/Apresentacao.java
@@ -4,10 +4,16 @@ public class Apresentacao {
 
     public void apresentar() {
         this.lucasSouza();
+        this.KarenGiullia();
     }
 
     private void lucasSouza() {
         System.out.println("");
+        System.out.println("Estou colaborando com o git");
+    }
+
+    private void KarenGiullia() {
+        System.out.println("Ola, sou a Karen");
         System.out.println("Estou colaborando com o git");
     }
 }
